c4d5bc5484e092e54c98cbbbf6e101a358dcae25
Andrew Fox <90468039+bobthecorn2000@users.noreply.github.com>
2023-05-11 07:53:12 -0600

    doug

---
+++ dist/assets/AboutView.3b49dcff.js
@@ -0,0 +1,1 @@
+import{a as e}from"./about.401b3a94.js";import{o,c as t,a}from"./index.287ed070.js";const s={class:"about"},p={__name:"AboutView",setup(c){return(r,_)=>(o(),t("div",s,[a(e)]))}};export{p as default};
